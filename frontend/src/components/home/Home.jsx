91cce2066f7fa570071f2eaafb5ce2e1b0d5c59f
Jeferson Fernandes <jfcristino.fernandes@gmail.com>
2019-09-16 01:06:18 -0300

    Mudando o parágrafo do componente Home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -5,5 +5,5 @@ export default props =>
     <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de React">
         <div className="display-4">Bem Vindo!</div>
         <hr />
-        <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+        <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React! Nosso Front-end feito em React vai consumir nosso Back-end com uma base de dados servido pelo JSON-Server e que foi subido ao ar com a utilização do Heroku</p>
     </Main>
